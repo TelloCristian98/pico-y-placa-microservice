ebeb92f39beb8302d5f98cb6f9162264c6ff7a01
Cristian Tello <cristian.andres.t98@gmail.com>
2025-02-13 12:15:20 -0500

    Initial project setup using Spring Initializr with basic Maven structure and essential dependencies

---
+++ src/main/java/com/CristianTello/picoyplaca/PicoYPlacaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.CristianTello.picoyplaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoYPlacaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoYPlacaMicroserviceApplication.class, args);
+	}
+
+}
