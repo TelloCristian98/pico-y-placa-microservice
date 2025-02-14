06dda069b58132927ff70693a998622022fa27ed
Cristian Tello <cristian.andres.t98@gmail.com>
2025-02-13 21:35:53 -0500

    Added PicoYPlacaService business logic for checking rules

--- src/main/java/com/CristianTello/picoyplaca/service/IServicePicoYPlaca.java
+++ src/main/java/com/CristianTello/picoyplaca/service/IServicePicoYPlaca.java
@@ -1,4 +1,8 @@
 package com.CristianTello.picoyplaca.service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public interface IServicePicoYPlaca {
+    boolean isVehicleAllowed(String licensePlate, LocalDate date, LocalTime time);
 }
--- src/main/java/com/CristianTello/picoyplaca/service/PicoYPlacaService.java
+++ src/main/java/com/CristianTello/picoyplaca/service/PicoYPlacaService.java
@@ -1,4 +1,49 @@
 package com.CristianTello.picoyplaca.service;
+import com.CristianTello.picoyplaca.model.CheckLog;
+import com.CristianTello.picoyplaca.model.PicoPlacaRule;
+import com.CristianTello.picoyplaca.repository.CheckLogRepository;
+import com.CristianTello.picoyplaca.repository.PicoPlacaRuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class PicoYPlacaService {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+@Service
+public class PicoYPlacaService implements IServicePicoYPlaca {
+
+    private final PicoPlacaRuleRepository ruleRepository;
+    private final CheckLogRepository logRepository;
+
+    @Autowired
+    public PicoYPlacaService(PicoPlacaRuleRepository ruleRepository, CheckLogRepository logRepository) {
+        this.ruleRepository = ruleRepository;
+        this.logRepository = logRepository;
+    }
+
+    @Override
+    public boolean isVehicleAllowed(String licensePlate, LocalDate date, LocalTime time) {
+        String dayName = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+
+        PicoPlacaRule rule = ruleRepository.findByDayOfWeek(dayName)
+                .orElseThrow(() -> new RuntimeException("No rule found for " + dayName));
+
+        char lastChar = licensePlate.charAt(licensePlate.length() - 1);
+        int lastDigit = Character.getNumericValue(lastChar);
+
+        boolean isInMorning = !time.isBefore(rule.getMorningStart()) && !time.isAfter(rule.getMorningEnd());
+        boolean isInEvening = !time.isBefore(rule.getEveningStart()) && !time.isAfter(rule.getEveningEnd());
+        boolean isRestricted = rule.getRestrictedDigits().contains(lastDigit) && (isInMorning || isInEvening);
+
+        CheckLog log = new CheckLog();
+        log.setLicensePlate(licensePlate);
+        log.setQueryDate(date);
+        log.setQueryTime(time);
+        log.setIsAllowed(!isRestricted);
+        logRepository.save(log);
+
+        return !isRestricted;
+    }
 }
