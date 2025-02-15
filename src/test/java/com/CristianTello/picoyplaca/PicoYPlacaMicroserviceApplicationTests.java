155f7e84149e31a7b8127e64c8826ca6f6922a0f
Cristian Tello <cristian.andres.t98@gmail.com>
2025-02-14 20:28:08 -0500

    Initial project setup using Spring Initializr with Maven structure and essential dependencies

---
+++ src/test/java/com/CristianTello/picoyplaca/PicoYPlacaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CristianTello.picoyplaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoYPlacaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
